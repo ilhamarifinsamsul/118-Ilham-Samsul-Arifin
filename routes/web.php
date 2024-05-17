ceedc1d9f1bb9da83b18eeacb74e161c7ae8b2e0
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2024-05-17 20:20:13 +0700

    modified file sidebar.blade.php, create route laporan and kategoribencana, last create controller KategoriBencana and Laporan

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KategoriBencana;
+use App\Http\Controllers\Laporan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/dashboard', function () {
@@ -9,3 +11,7 @@ Route::get('/dashboard', function () {
 Route::get('/laporanview', function () {
     return view('pages.laporanview.index');
 });
+
+Route::resource('laporan', Laporan::class);
+
+Route::resource('kategoribencana', KategoriBencana::class);
